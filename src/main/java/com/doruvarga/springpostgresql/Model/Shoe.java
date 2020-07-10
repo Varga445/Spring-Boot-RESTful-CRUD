d7ac87ee388e0ec0803c1f5bcedb84cca0e5e439
Varga445 <51417234+Varga445@users.noreply.github.com>
2020-07-10 18:41:25 +0300

    Update Shoe.java

--- src/main/java/com/doruvarga/springpostgresql/Model/Shoe.java
+++ src/main/java/com/doruvarga/springpostgresql/Model/Shoe.java
@@ -3,44 +3,20 @@ package com.doruvarga.springpostgresql.Model;
 
 import javax.persistence.*;
 
-//asa configurezi o entitate jpa
+
 @Entity
 @Table(name = "shoes")
 public class Shoe {
 
-    @Id//asa faci sa ai primary key-ul
+    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    //@GeneratedValue(strategy = GenerationType.IDENTITY) !!! nu mere asa iti da eroaraea asta
-    //ConstraintViolationException
-    //asa faci a primary key-ul sa sea auto incrementeze cand adaugi un element
-    //Pentru POSTGRES
+
     private int id;
 
-    ///////////////////////////////////////////////////////
-//   @ManyToOne
-//    private Outfit outfit;
-//
-//    public Outfit getOutfit() {
-//        return outfit;
-//    }
-//
-//    public void setOutfit(Outfit outfit) {
-//        this.outfit = outfit;
-//    }
-    //
-//
-//    public Outfit getOutfit() {
-//        return outfit;
-//    }
-//
-//    public void setOutfit(Outfit outfit) {
-//        this.outfit = outfit;
-//    }
-
-    //////////////////////////////////////////////////////////////
+ 
     @Column(name = "model_name")
     private String modelName;
-    //asa creezi in tabel o coloana denumita model_name
+  
 
 
     @Column(name = "brand_name")
