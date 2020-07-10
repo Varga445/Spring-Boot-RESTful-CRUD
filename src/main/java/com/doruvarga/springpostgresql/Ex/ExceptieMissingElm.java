559f0297513ee5d002c7f362c96c4f8181030845
Varga445 <51417234+Varga445@users.noreply.github.com>
2020-07-10 18:40:06 +0300

    Update ExceptieMissingElm.java

--- src/main/java/com/doruvarga/springpostgresql/Ex/ExceptieMissingElm.java
+++ src/main/java/com/doruvarga/springpostgresql/Ex/ExceptieMissingElm.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ExceptieMissingElm extends Exception {
 
-
-    //private static final int serialVersionUID = 1;
-
     public ExceptieMissingElm(String mesaj) {
 
         super(mesaj);
