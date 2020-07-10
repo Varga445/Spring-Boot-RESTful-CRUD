f34258acdc7a58aa9722bfb51be1b1c3f2387aea
Varga445 <51417234+Varga445@users.noreply.github.com>
2020-07-10 18:42:32 +0300

    Update ShoeRepository.java

--- src/main/java/com/doruvarga/springpostgresql/Repository/ShoeRepository.java
+++ src/main/java/com/doruvarga/springpostgresql/Repository/ShoeRepository.java
@@ -4,8 +4,6 @@ import com.doruvarga.springpostgresql.Model.Shoe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-//asa transformi interfata asta intr-o componenta
 @Repository
 public interface ShoeRepository extends JpaRepository<Shoe, Integer> {
 
