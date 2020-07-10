e0da2560d8c18db0a9d84a10059477d4d11cd0e8
Varga445 <51417234+Varga445@users.noreply.github.com>
2020-07-10 18:42:13 +0300

    Update OutfitRepository.java

--- src/main/java/com/doruvarga/springpostgresql/Repository/OutfitRepository.java
+++ src/main/java/com/doruvarga/springpostgresql/Repository/OutfitRepository.java
@@ -5,7 +5,6 @@ import com.doruvarga.springpostgresql.Model.Outfit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//asa transformi interfata asta intr-o componenta
 @Repository
 public interface OutfitRepository extends JpaRepository<Outfit,Integer> {
 
